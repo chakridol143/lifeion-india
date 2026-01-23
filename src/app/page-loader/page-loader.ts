8407c1d907e1f2acf56860d64dfdff6741ce82fa
Ravi-kish <nallaravikishore@gmail.com>
2026-01-23 18:50:13 +0530

    Maharastra

---
+++ src/app/page-loader/page-loader.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-loader.html',
+  styleUrl: './page-loader.css',
+})
+export class PageLoader {
+
+}
